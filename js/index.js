ee93954609c1341ebcaf652b2010d2460e601ab9
GalaxyCaramel <eunha05@kakao.com>
2023-07-03 22:44:17 +0900

    made two timers

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
-const clock = document.getElementsByClassName("RealTimeClock");
+const clock = document.getElementsByClassName("real-time-clock")[0];
+const clock2 = document.getElementsByClassName("real-time-clock-2")[0];
 
-const DDay = new Date("2024-01-10T08:30+09:00");
-
-const calculate = () => {
+const calculate = (dday) => {
     const now = new Date()
-    const gap = DDay.getTime() - now.getTime()
+    const gap = dday.getTime() - now.getTime()
 
     const day = Math.floor(gap / (1000 * 60 * 60 * 24))
     const hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
@@ -15,9 +14,11 @@ const calculate = () => {
 }
 
 const render = () => {
-    const [day, hours, minutes, seconds] = calculate();
+    const [day, hours, minutes, seconds] = calculate(new Date("2024-01-10T08:30+09:00"));
+    const [day2, hours2, minutes2, seconds2] = calculate(new Date("2024-01-10+09:00"));
 
-    clock[0].innerHTML = `그가 집에 가기까지 ${day}일 ${hours}시간 ${minutes}분 ${seconds}초 남았습니다.`;
+    clock.innerHTML = `그가 대대출발까지 ${day}일 ${hours}시간 ${minutes}분 ${seconds}초 남았습니다.`;
+    clock2.innerHTML = `그가 집에 가는 날까지 ${day2}일 ${hours2}시간 ${minutes2}분 ${seconds2}초 남았습니다.`;
 }
 
 setInterval(render, 1000);
