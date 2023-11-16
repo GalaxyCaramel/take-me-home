8de5d075fb6eef2b2173f2bb6f90c7127d7b60f2
GalaxyCaramel <eunha05@kakao.com>
2023-11-16 19:02:12 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ const render = () => {
 
     clock.innerHTML = `부대 출발까지 ${day}일 ${hours}시간 ${minutes}분 ${seconds}초 남았습니다.`;
     clock2.innerHTML = `그가 집에 가는 날까지 ${day2}일 ${hours2}시간 ${minutes2}분 ${seconds2}초 남았습니다.`;
-    clock3.innerHTML = `마지막휴가까지 ${day3}일 ${hours3}시간 ${minutes3}분 ${seconds3}초 남았습니다.`;
+    clock3.innerHTML = `마지막 휴가까지 ${day3}일 ${hours3}시간 ${minutes3}분 ${seconds3}초 남았습니다.`;
 }
 
 setInterval(render, 1000);
