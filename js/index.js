2953d8739c0ba74b4344dc8076162a0e031c7762
GalaxyCaramel <eunha05@kakao.com>
2024-01-11 03:31:13 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,15 +14,26 @@ const calculate = (dday) => {
     return [day, hours, minutes, seconds]
 }
 
+const calculate2 = (dday) => {
+    const now = new Date()
+    const gap = now.getTime() - dday.getTime()
+
+    const day = Math.floor(gap / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((gap % (1000 * 60)) / 1000)
+
+    return [day, hours, minutes, seconds]
+}
+
 const render = () => {
-    const [day, hours, minutes, seconds] = calculate(new Date("2024-01-10T00:00+09:00"));
+    const [day, hours, minutes, seconds] = calculate2(new Date("2024-01-10T00:00+09:00"));
     const [day2, hours2, minutes2, seconds2] = calculate(new Date("2025-01-01T00:00+09:00"));
-    const [day3, hours3, minutes3, seconds3] = calculate(new Date("2024-01-09T20:00+09:00"));
+
 
 
     clock.innerHTML = `전역한지 ${day}일 ${hours}시간 ${minutes}분 ${seconds}초 되었습니다.`;
     clock2.innerHTML = `예비역 1년차까지 ${day2}일 ${hours2}시간 ${minutes2}분 ${seconds2}초 남았습니다.`;
-    clock3.innerHTML = `마지막 휴가 복귀까지 ${day3}일 ${hours3}시간 ${minutes3}분 ${seconds3}초 남았습니다.`;
 }
 
 setInterval(render, 1000);
