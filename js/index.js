650f73d2897bee174ed1e3a51c15e45174603a58
GalaxyCaramel <eunha05@kakao.com>
2024-01-11 02:22:20 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,13 +15,13 @@ const calculate = (dday) => {
     return [day, hours, minutes, seconds]
 
 
-    const now = new Date1()
+    const now1 = new Date1()
     const gap1 = dday1.getTime() - now.getTime()
 
-    const day = Math.floor(gap1 / (1000 * 60 * 60 * 24))
-    const hours = Math.floor((gap1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    const minutes = Math.floor((gap1 % (1000 * 60 * 60)) / (1000 * 60))
-    const seconds = Math.floor((gap1 % (1000 * 60)) / 1000)
+    const day1 = Math.floor(gap1 / (1000 * 60 * 60 * 24))
+    const hours1 = Math.floor((gap1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes1 = Math.floor((gap1 % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds1 = Math.floor((gap1 % (1000 * 60)) / 1000)
 
 
     return [dday, dhours, dminutes, dseconds]
@@ -29,7 +29,7 @@ const calculate = (dday) => {
 
 const render = () => {
     const [day, hours, minutes, seconds] = calculate(new Date("2024-01-10T00:00+09:00"));
-    const [day2, hours2, minutes2, seconds2] = calculate(new Date1("2025-01-01T00:00+09:00"));
+    const [day1, hours1, minutes1, seconds1] = calculate(new Date1("2025-01-01T00:00+09:00"));
     const [day3, hours3, minutes3, seconds3] = calculate(new Date("2024-01-09T20:00+09:00"));
 
 
