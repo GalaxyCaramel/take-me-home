5af701b218aea52309aadf099b1e4b90994bdeea
GalaxyCaramel <eunha05@kakao.com>
2025-01-06 04:40:36 +0900

    예비역 연도 변경

--- js/index.js
+++ js/index.js
@@ -39,12 +39,12 @@ const calculateSincePast = (dday) => {
 
 const render = () => {
     const [day, hours, minutes, seconds] = calculateTotal(new Date("2024-01-10T00:00+09:00"));
-    const [day2, hours2, minutes2, seconds2] = calculateTotal(new Date("2025-01-01T00:00+09:00"));
+    const [day2, hours2, minutes2, seconds2] = calculateTotal(new Date("2026-01-01T00:00+09:00"));
 
 
 
     clock.innerHTML = `전역한지 ${day}일 ${hours}시간 ${minutes}분 ${seconds}초 되었습니다.`;
-    clock2.innerHTML = `예비역 1년차까지 ${day2}일 ${hours2}시간 ${minutes2}분 ${seconds2}초 남았습니다.`;
+    clock2.innerHTML = `예비역 2년차까지 ${day2}일 ${hours2}시간 ${minutes2}분 ${seconds2}초 남았습니다.`;
 }
 
 setInterval(render, 1000);
